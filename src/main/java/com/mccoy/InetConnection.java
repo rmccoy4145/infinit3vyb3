4a2fc9e0a2db4baa5e2df879fdb91d867d8ff93f
Ryan McCoy <ryan.mccoy85@gmail.com>
2024-08-25 17:33:22 -0400

    create InetConnection to create handle interface selection

---
+++ src/main/java/com/mccoy/InetConnection.java
@@ -0,0 +1,58 @@
+package com.mccoy;
+
+import java.net.*;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class InetConnection {
+    private static final Logger LOG = Logger.getLogger(InetConnection.class.getName());
+
+    private InetAddress inetAddress;
+    private NetworkInterface networkInterface;
+
+    InetConnection() throws SocketException {
+        this.setInet();
+    }
+
+    public void setInet() throws SocketException {
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = interfaces.nextElement();
+
+            // Skip loopback and down interfaces
+            if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+                continue;
+            }
+
+            // Check if the interface supports multicast
+            if (networkInterface.supportsMulticast()) {
+                this.networkInterface = networkInterface;
+                List<InterfaceAddress> addresses = networkInterface.getInterfaceAddresses();
+
+                for(InterfaceAddress address : addresses) {
+                    InetAddress inetAddress = address.getAddress();
+                    // Skip IPv6 loopback address (::1) if you prefer an IPv4 address
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        this.inetAddress = inetAddress;
+                        return;
+                    }
+                }
+            }
+        }
+
+        // If no suitable interface is found
+        if (this.inetAddress == null || this.inetAddress.isLoopbackAddress()) {
+            LOG.severe("No valid network interface found");
+        }
+    }
+
+    public InetAddress getInetAddress() {
+        return inetAddress;
+    }
+
+    public NetworkInterface getNetworkInterface() {
+        return networkInterface;
+    }
+}
